648f70b59130c3e3e3ad723717a8d93e33593b0d
kubarazny <kontakt@kubarazny.pl>
2019-05-07 21:11:21 +0200

    init vue project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>TodoApp</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
